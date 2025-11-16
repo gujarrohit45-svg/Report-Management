88672e5e022d5a4729f2049b9e70e91ecedf8e08
gujarrohit45-svg <gujarrohit45@gmail.com>
2025-11-16 17:45:58 +0530

    Added Allowed hosts.

--- ReportManagement/settings.py
+++ ReportManagement/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'report-management-t9qm.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
+
 
 
 # Application definition
